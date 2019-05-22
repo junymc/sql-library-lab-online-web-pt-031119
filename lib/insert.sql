9a020e9ebd4ca569e837ec94fcc5ae13a48b2cfe
Junyoung McArdle <juny.mcardle@gmail.com>
2019-05-22 04:14:48 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -16,7 +16,6 @@ INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES
 (3, "Daenerys Targaryen", "If I look back I am lost", "human", 1, 1),
 (4, "Eddard Stark", "Winter is coming", "human", 1, 1),
 (5, "Character One", "motto one", "cylon", 2, 2),
-(6, "Character Two", "motto two", "human", 2, 2), 
-(7, "Character Three", "motto three", "cylon", 2, 2), 
+(6, "Character Two", "motto two", "human", 2, 2),
+(7, "Character Three", "motto three", "cylon", 2, 2),
 (8, "Character Four", "motto four", "cylon", 2, 2);
-
