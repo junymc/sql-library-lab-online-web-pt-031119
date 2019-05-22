9d1650180b88c33b73339f85a377f31c39f98c66
Junyoung McArdle <juny.mcardle@gmail.com>
2019-05-22 04:37:45 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -23,7 +23,7 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT authors.name, subgenres.name 
+  "SELECT authors.name, subgenres.name
   FROM series
   JOIN authors
   ON series.author_id = authors.id
