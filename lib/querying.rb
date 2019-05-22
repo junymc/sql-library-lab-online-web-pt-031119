8d399807484a5bdc87f854d7feecd440d699edde
Junyoung McArdle <juny.mcardle@gmail.com>
2019-05-22 04:49:34 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -47,5 +47,5 @@ def select_character_names_and_number_of_books_they_are_in
   FROM character_books
   JOIN characters
   ON character_books.character_id = characters.id
-  ORDER BY COUNT(name);"
+  ORDER BY COUNT(name) DESC;"
 end
