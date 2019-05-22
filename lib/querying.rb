3cf1c60cfcd189176a731257e3875bda7011939f
Junyoung McArdle <juny.mcardle@gmail.com>
2019-05-22 04:44:12 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -32,11 +32,14 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT title
+  "SELECT series.title
   FROM characters
   JOIN series
   ON characters.series_id = series.id
-  "
+  GROUP BY series.title
+  HAVING characters.species = 'human'
+  ORDER BY COUNT(species) DESC
+  LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
